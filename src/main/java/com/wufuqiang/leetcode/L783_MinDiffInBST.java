55157f937c8cdc2b2c8c82724b99832d1e88ed07
wufq <wufq@auto_create>
2020-01-02 19:07:58 +0800

    L783

---
+++ src/main/java/com/wufuqiang/leetcode/L783_MinDiffInBST.java
@@ -0,0 +1,13 @@
+package com.wufuqiang.leetcode;
+
+import com.wufuqiang.leetcode.entries.TreeNode;
+
+public class L783_MinDiffInBST {
+    public void inOrder(TreeNode root){
+
+    }
+    public int minDiffInBST(TreeNode root) {
+
+        return 0;
+    }
+}
---
+++ src/main/java/com/wufuqiang/leetcode/traversal/BinaryTreeTraversal.java
@@ -0,0 +1,14 @@
+package com.wufuqiang.leetcode.traversal;
+
+import com.wufuqiang.leetcode.entries.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreeTraversal {
+    public void inOrder(TreeNode root){
+        List<Integer> result = new ArrayList<Integer>();
+
+
+    }
+}
