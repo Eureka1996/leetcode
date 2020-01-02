43f745049256fb6b70c4c845ff4b55a73494a83a
wufq <wufq@auto_create>
2020-01-02 19:24:41 +0800

    L783

--- src/main/java/com/wufuqiang/leetcode/L783_MinDiffInBST.java
+++ src/main/java/com/wufuqiang/leetcode/L783_MinDiffInBST.java
@@ -2,12 +2,38 @@ package com.wufuqiang.leetcode;
 
 import com.wufuqiang.leetcode.entries.TreeNode;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
 public class L783_MinDiffInBST {
     public void inOrder(TreeNode root){
 
     }
     public int minDiffInBST(TreeNode root) {
-
-        return 0;
+        if(root == null){
+            return 0;
+        }
+        int result = Integer.MAX_VALUE;
+        boolean isFirstElement = true;
+        int preEle = 0;
+        Stack<TreeNode> stack = new Stack<>();
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.push(root);
+                root =root.left;
+            }
+            root = stack.pop();
+            if(isFirstElement){
+                preEle = root.val;
+            }else{
+                if(root.val - preEle < result){
+                    result = root.val-preEle;
+                }
+                preEle = root.val;
+            }
+            root = root.right;
+        }
+        return result;
     }
 }
--- src/main/java/com/wufuqiang/leetcode/traversal/BinaryTreeTraversal.java
+++ src/main/java/com/wufuqiang/leetcode/traversal/BinaryTreeTraversal.java
@@ -4,11 +4,24 @@ import com.wufuqiang.leetcode.entries.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreeTraversal {
-    public void inOrder(TreeNode root){
+    public List<Integer> inOrder(TreeNode root){
         List<Integer> result = new ArrayList<Integer>();
-
-
+        if(root == null){
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        while(root != null || !stack.isEmpty()){
+            while(root != null){
+                stack.push(root);
+                root =root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
     }
 }
