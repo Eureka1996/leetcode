816079e2792ef6eeb717bc8410811df04736f543
wufq <wufq@auto_create>
2020-01-03 16:37:54 +0800

    L988

---
+++ src/main/java/com/wufuqiang/leetcode/L988_SmallestFromLeaf.java
@@ -0,0 +1,10 @@
+package com.wufuqiang.leetcode;
+
+import com.wufuqiang.leetcode.entries.TreeNode;
+
+public class L988_SmallestFromLeaf {
+    public String smallestFromLeaf(TreeNode root) {
+
+        return "";
+    }
+}
