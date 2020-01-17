3a8e93b627f91a69eff27aa4a3870636873e8c40
eureka_china <1767778344@qq.com>
2020-01-17 22:01:39 +0800

    L239

---
+++ src/main/java/com/wufuqiang/leetcode/L155_MinStack.java
@@ -0,0 +1,54 @@
+package com.wufuqiang.leetcode;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * @ author wufuqiang
+ **/
+public class L155_MinStack {
+}
+
+class MinStack {
+
+    Stack<Integer> data ;
+    Stack<Integer> helper ;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        data = new Stack<Integer>();
+        helper = new Stack<Integer>();
+    }
+
+    public void push(int x) {
+        data.add(x);
+        if(helper.isEmpty() || helper.peek()>=x){
+            helper.add(x);
+        }
+    }
+
+    public void pop() {
+        if(!data.isEmpty()){
+            int top = data.pop();
+            if(top == helper.peek()){
+                helper.pop();
+            }
+        }
+
+    }
+
+    public int top() {
+        if(!data.isEmpty()){
+            return data.peek();
+        }
+        throw new RuntimeException("栈为空");
+
+    }
+
+    public int getMin() {
+        if(!helper.isEmpty()){
+            return helper.peek();
+        }
+        throw new RuntimeException("栈为空");
+    }
+}
