f0244ba1eda0101e564dc24df599ec146ce5d1a7
wufq <wufq@auto_create>
2020-01-17 19:28:40 +0800

    L869

--- src/main/java/com/wufuqiang/leetcode/L869_ReorderedPowerOf2.java
+++ src/main/java/com/wufuqiang/leetcode/L869_ReorderedPowerOf2.java
@@ -1,5 +1,14 @@
 package com.wufuqiang.leetcode;
 
 public class L869_ReorderedPowerOf2 {
+    public boolean reorderedPowerOf2(int N) {
+        return false;
 
+    }
+
+    public int[] count(int N){
+        while(N>0){
+            int mod = N%10;
+        }
+    }
 }
