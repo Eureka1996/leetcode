0fab47b9e96351762acf20c74a76143d808d8214
eureka_china <1767778344@qq.com>
2020-01-17 23:23:08 +0800

    L869

--- src/main/java/com/wufuqiang/leetcode/L869_ReorderedPowerOf2.java
+++ src/main/java/com/wufuqiang/leetcode/L869_ReorderedPowerOf2.java
@@ -1,15 +1,25 @@
 package com.wufuqiang.leetcode;
 
+import java.util.Arrays;
+
 public class L869_ReorderedPowerOf2 {
     public boolean reorderedPowerOf2(int N) {
+        int[] A = count(N);
+        for(int i = 0 ; i<31;i++){
+            if(Arrays.equals(A,count(1<<i))){
+                return true;
+            }
+        }
         return false;
-
     }
 
-//    public int[] count(int N){
-//        while(N>0){
-//            int mod = N%10;
-//        }
-//        return null;
-//    }
+    public int[] count(int N){
+        int[] count = new int[10];
+        while(N>0){
+            int mod = N%10;
+            count[mod] = count[mod]+1;
+            N /= 10;
+        }
+        return count;
+    }
 }
