0837ad2c4d11e94dc0415c375a1755a7ac8e2d29
wufq <wufq@auto_create>
2020-01-21 17:07:16 +0800

    L77

---
+++ src/main/java/com/wufuqiang/leetcode/L77_Combine.java
@@ -0,0 +1,13 @@
+package com.wufuqiang.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class L77_Combine {
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        return result;
+    }
+}
