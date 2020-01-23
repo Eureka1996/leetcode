9b077bd5a961fc52bb6ce333a9bdfa66afd13630
eureka_china <1767778344@qq.com>
2020-01-23 22:42:06 +0800

    L1291未完成

---
+++ src/main/java/com/wufuqiang/leetcode/L1291_SequentialDigits.java
@@ -0,0 +1,18 @@
+package com.wufuqiang.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ author wufuqiang
+ **/
+public class L1291_SequentialDigits {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        
+
+
+        return result;
+    }
+}
