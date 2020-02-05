67b53cc5cd2b1b28821a0916949d25238717b490
eureka_china <1767778344@qq.com>
2020-02-05 15:59:41 +0800

    添加parent属性，指向你节点

--- src/main/java/com/wufuqiang/leetcode/entries/TreeNode.java
+++ src/main/java/com/wufuqiang/leetcode/entries/TreeNode.java
@@ -3,6 +3,7 @@ public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
+    public TreeNode parent;
     public TreeNode(int val){
         this.val =val;
     }
