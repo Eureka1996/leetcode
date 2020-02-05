ee9a7c19e0796c29177b80848c84a1f894098313
eureka_china <1767778344@qq.com>
2020-02-05 17:05:27 +0800

    二叉搜索树的remove方法测试

--- src/test/java/com/wufuqiang/test/BSTTest.java
+++ src/test/java/com/wufuqiang/test/BSTTest.java
@@ -20,4 +20,25 @@ public class BSTTest {
         TreeNode searched = BST.search(root,129);
         System.out.println(searched);
     }
+
+    @Test
+    public void removeTest(){
+        TreeNode root = new TreeNode(15);
+        BST.insert(root,new TreeNode(6));
+        BST.insert(root,new TreeNode(18));
+        BST.insert(root,new TreeNode(3));
+        BST.insert(root,new TreeNode(7));
+        BST.insert(root,new TreeNode(17));
+        BST.insert(root,new TreeNode(20));
+        BST.insert(root,new TreeNode(2));
+        BST.insert(root,new TreeNode(4));
+        BST.insert(root,new TreeNode(13));
+        BST.insert(root,new TreeNode(9));
+        System.out.println(travel.inOrder(root));
+        TreeNode searched = BST.search(root, 13);
+        System.out.println(searched);
+        TreeNode successor = BST.successor(searched);
+        System.out.println(successor);
+
+    }
 }
