09a303f6aac8066f08c3b145bc1039adb14d0c5e
eureka_china <1767778344@qq.com>
2020-02-06 10:04:43 +0800

    AVL树插入操作

--- src/main/java/com/wufuqiang/leetcode/tree/AVL.java
+++ src/main/java/com/wufuqiang/leetcode/tree/AVL.java
@@ -6,6 +6,14 @@ import com.wufuqiang.leetcode.entries.AVLNode;
  * @ author wufuqiang
  **/
 public class AVL {
+
+    private int getHeight(AVLNode<Integer> node){
+        if(node != null){
+            return node.height;
+        }
+        return 0;
+    }
+
     /**
      * 左左单旋
      * @param node
@@ -16,8 +24,8 @@ public class AVL {
         AVLNode<Integer> leftRight = left.right;
         node.left = leftRight;
         left.right = node;
-        node.height = Math.max(node.left.height,node.right.height)+1;
-        left.height = Math.max(left.left.height,left.right.height)+1;
+        node.height = Math.max(getHeight(node.left),getHeight(node.right))+1;
+        left.height = Math.max(getHeight(left.left),getHeight(left.right))+1;
         return left;
     }
 
@@ -30,8 +38,8 @@ public class AVL {
         AVLNode<Integer> right = node.right;
         node.right = right.left;
         right.left = right;
-        node.height = Math.max(node.left.height,node.right.height)+1;
-        right.height = Math.max(right.left.height,right.right.height)+1;
+        node.height = Math.max(getHeight(node.left),getHeight(node.right))+1;
+        right.height = Math.max(getHeight(right.left),getHeight(right.right))+1;
         return right;
     }
 
@@ -45,11 +53,40 @@ public class AVL {
         return singleRotateLeft(node);
     }
 
+    /**
+     * 右左双旋
+     * @param node
+     * @return
+     */
     private AVLNode<Integer> doubleRotateWithRight(AVLNode<Integer> node){
-        node.right =
+        node.right = singleRotateLeft(node.right);
+        return singleRotateRight(node);
     }
 
+    public AVLNode<Integer> insert(AVLNode<Integer> root,int data){
+        if(root == null){
+            root = new AVLNode<Integer>(data);
+        }else if(data < root.data){
+            root.left = insert(root.left,data);
+            if(getHeight(root.left)-getHeight(root.right) == 2){
+                if(data < root.left.data){
+                    root = singleRotateLeft(root);
+                }else{
+                    root = doubleRotateWithLeft(root);
+                }
+            }
 
-
-
+        }else if(data > root.data){
+            root.right = insert(root.right,data);
+            if(getHeight(root.right) - getHeight(root.left) == 2){
+                if(data < root.right.data){
+                    root = doubleRotateWithRight(root);
+                }else{
+                    root = singleRotateRight(root);
+                }
+            }
+        }
+        root.height = Math.max(getHeight(root.left),getHeight(root.right))+1;
+        return root;
+    }
 }
