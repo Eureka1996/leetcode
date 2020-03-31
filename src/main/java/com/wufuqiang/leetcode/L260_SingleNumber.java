9567388e43eeff0354b01af18d662ef35e037c30
Eureka1996 <1767778344@qq.com>
2020-03-31 11:36:02 +0800

    L260

---
+++ src/main/java/com/wufuqiang/leetcode/L260_SingleNumber.java
@@ -0,0 +1,21 @@
+package com.wufuqiang.leetcode;
+
+public class L260_SingleNumber {
+	public int[] singleNumber(int[] nums) {
+		int bitmask = 0;
+		//获取两个只出现一次的数字的异或值
+		for(int i : nums){
+			bitmask ^= i;
+		}
+		//获取最右边的1
+		int diff = bitmask & (-bitmask);
+		int x = 0;
+		for(int i : nums){
+			if((i&diff)!=0){
+				x^=i;
+			}
+		}
+		int y = bitmask ^ x;
+		return new int[]{x,y};
+	}
+}
