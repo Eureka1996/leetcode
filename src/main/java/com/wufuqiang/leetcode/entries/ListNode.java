4cb2fb167bc0a125a41324d83c9139a586756405
wufq <wufq@auto_create>
2020-04-14 17:39:23 +0800

    ListNode

---
+++ src/main/java/com/wufuqiang/leetcode/entries/ListNode.java
@@ -0,0 +1,10 @@
+package com.wufuqiang.leetcode.entries;
+
+public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) {
+          val = x;
+          next = null;
+      }
+}
