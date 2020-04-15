c53689b0c3de8243f96c0a57b6dfc6b617b4dd06
wufq <wufq@auto_create>
2020-04-15 11:20:02 +0800

    L77

--- src/main/java/com/wufuqiang/leetcode/L77_Combine.java
+++ src/main/java/com/wufuqiang/leetcode/L77_Combine.java
@@ -2,12 +2,27 @@ package com.wufuqiang.leetcode;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//组合
 public class L77_Combine {
 
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-
+        List<Integer> path = new ArrayList<>();
+        backtrack(result,path,1,n,k);
         return result;
+    }
+
+    public void backtrack(List<List<Integer>> res,List<Integer> path,int start,int n,int k){
+        if(path.size() == k){
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = start ; i<=n;i++){
+            if(!path.contains(i)){
+                path.add(i);
+                backtrack(res,path,i+1,n,k);
+                path.remove(path.size()-1);
+            }
+        }
     }
 }
