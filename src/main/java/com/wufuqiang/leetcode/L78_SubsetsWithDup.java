8af0506278de79e391fdcad3d765f86ed1c1de08
wufq <wufq@auto_create>
2020-04-15 16:43:55 +0800

    L78

---
+++ src/main/java/com/wufuqiang/leetcode/L78_SubsetsWithDup.java
@@ -0,0 +1,28 @@
+package com.wufuqiang.leetcode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//子集
+public class L78_SubsetsWithDup {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(result,path,nums,0);
+        return result;
+    }
+
+    public void backtrack(List<List<Integer>> res,List<Integer> path,int[] nums,int begin){
+
+        res.add(new ArrayList<>(path));
+
+        for(int i = begin;i<nums.length;i++){
+            if(i>begin && nums[i]  == nums[i-1]) continue;
+            path.add(nums[i]);
+            backtrack(res,path,nums,i+1);
+            path.remove(path.size()-1);
+        }
+    }
+}
