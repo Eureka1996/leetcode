de220e7ce053616b27314fa410365cfcdfa158ea
wufq <wufq@auto_create>
2020-04-17 13:30:18 +0800

    MS08_01

--- src/main/java/com/wufuqiang/leetcode/MS08_01_WaysToStep.java
+++ src/main/java/com/wufuqiang/leetcode/MS08_01_WaysToStep.java
@@ -4,18 +4,23 @@ public class MS08_01_WaysToStep {
 
 	public static void main(String[] args) {
 		MS08_01_WaysToStep ms08 = new MS08_01_WaysToStep();
-		int i = ms08.waysToStep(3);
+		int i = ms08.waysToStep(61);
+		System.out.println(Integer.MAX_VALUE*1.0/1000000007);
 		System.out.println(i);
 	}
 	public int waysToStep(int n) {
-		int[] dp = new int[n+3];
+		if(n==1) return 1;
+		if(n==2) return 2;
+		int[] dp = new int[n];
 		dp[0] = 1;
-		dp[1] = 1;
-		dp[2] = 1;
-		for(int i = 3;i<n+3;i++){
-			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		dp[1] = 2;
+		dp[2] = 4;
+		for(int i = 3;i<n;i++){
+			dp[i] = ((dp[i-1] + dp[i-2])%(1000000007) + dp[i-3])%(1000000007);
 		}
 
-		return dp[n+2];
+//		752119970
+
+		return dp[n-1];
 	}
 }
