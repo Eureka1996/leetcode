013e989350a11c4f4ffaec9d362d11c3712d3c54
Eureka1996 <1767778344@qq.com>
2020-04-23 12:09:01 +0800

    L1315

---
+++ src/main/java/com/wufuqiang/leetcode/L1315_SumEvenGrandparent.java
@@ -0,0 +1,20 @@
+package com.wufuqiang.leetcode;
+
+import com.wufuqiang.leetcode.entries.TreeNode;
+//祖父节点有偶数
+public class L1315_SumEvenGrandparent {
+	int result = 0;
+	public int sumEvenGrandparent(TreeNode root) {
+		dfs(null,null,root);
+
+		return result;
+	}
+
+	public void dfs(TreeNode grandparent,TreeNode parent,TreeNode current){
+		if(current != null){
+			if(grandparent != null && grandparent.val % 2 == 0) result+=current.val;
+			if(current.left != null) dfs(parent,current,current.left);
+			if(current.right != null) dfs(parent,current,current.right);
+		}
+	}
+}
