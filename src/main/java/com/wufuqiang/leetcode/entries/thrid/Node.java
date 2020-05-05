4eeffff9942037f1c914025678c783a62b8b84d1
Eureka1996 <1767778344@qq.com>
2020-05-05 13:10:26 +0800

    复杂链表节点

---
+++ src/main/java/com/wufuqiang/leetcode/entries/thrid/Node.java
@@ -0,0 +1,13 @@
+package com.wufuqiang.leetcode.entries.thrid;
+//复杂链表节点
+public class Node {
+	public int val;
+	public Node next;
+	public Node random;
+
+	public Node(int val) {
+		this.val = val;
+		this.next = null;
+		this.random = null;
+	}
+}
