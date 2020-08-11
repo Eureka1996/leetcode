a482ebf069200ab189c280cd77b85ba920e5ddcf
1527198976RIJ <1527198976rij@kuaishou.com>
2020-08-11 17:44:41 +0800

    QuickSort

--- src/main/java/com/wufuqiang/leetcode/sort/QuickSort.java
+++ src/main/java/com/wufuqiang/leetcode/sort/QuickSort.java
@@ -2,6 +2,10 @@ package com.wufuqiang.leetcode.sort;
 
 import java.util.Arrays;
 
+/**
+ * 快排
+ * 从小到大排序
+ */
 public class QuickSort {
     public static void quickSort(int A[],int low ,int hight){
         if(low < hight){
@@ -11,10 +15,10 @@ public class QuickSort {
         }
     }
     public static int partition(int A[] , int low , int hight){
-        int i = low -1;  //i的起始位置
-        int x = A[hight];
+        int i = low -1;  //i的起始位置，是最低位置的前一个位置，表示小于x的最高位置
+        int x = A[hight];//选定的比较位置
         for(int j = low ; j < hight ; j++){
-            if(A[j] >= x){ //判断条件
+            if(A[j] <= x){ //判断条件，小于或等于指定值的数据需要做交换
                 i = i+1;
                 int tmp = A[i];
                 A[i] = A[j];
@@ -28,7 +32,7 @@ public class QuickSort {
     }
 
     public static void main(String[] args){
-        int a[] = { 49, 38, 65, 97, 76, 13, 27, 49 };
+        int a[] = { 1,2,3,4,5,49, 38, 65, 97, 76, 13, 27, 49 };
         quickSort(a,0,a.length-1);
         System.out.println(Arrays.toString(a));
     }
