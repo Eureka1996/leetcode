d879be1a1b3a3ed335c8378c2837be0761fa9130
1527198976RIJ <1527198976rij@kuaishou.com>
2020-08-13 16:52:50 +0800

    汉诺塔，hanoi

--- src/main/java/com/wufuqiang/kuaishuo/Hanoi.java
+++ src/main/java/com/wufuqiang/kuaishuo/Hanoi.java
@@ -4,7 +4,7 @@ package com.wufuqiang.kuaishuo;
  * 汉诺塔
  */
 public class Hanoi {
-	private static int i = 0;//记录执行的次数
+	private static int i = 0;//记录执行的次数，需要搬运多少次
 
 	public static void main(String[] args) {
 
@@ -16,13 +16,20 @@ public class Hanoi {
 		System.out.println("第"+(++i)+"步，将第"+num+"个盘子从"+a+"移到到"+c);
 	}
 
+	/**
+	 *
+	 * @param num 盘子个数
+	 * @param a 源位置
+	 * @param b 中间位置，过渡位置
+	 * @param c 目的位置
+	 */
 	public static void hannuota(int num ,String a,String b ,String c){
 		if(num ==1){
-			move(num,a,c);
+			move(num,a,c); //一个盘子时，直接从a移动到b
 		}else{
-			hannuota(num-1,a,c,b);
-			move(num,a,c);
-			hannuota(num-1,b,a,c);
+			hannuota(num-1,a,c,b);//将num-1个盘子从a移动到b，借助c
+			move(num,a,c);//将当前最大的盘子num从a移动到c
+			hannuota(num-1,b,a,c);//将num-1个盘子从b移动到c，借助a
 		}
 
 	}
