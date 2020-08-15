87c4ca36be2b8dd72fb71a50a5724604c7588413
1527198976RIJ <1527198976rij@kuaishou.com>
2020-08-15 16:38:53 +0800

    L81_搜索旋转数组2，存在重复元素

--- src/main/java/com/wufuqiang/leetcode/L81_Search.java
+++ src/main/java/com/wufuqiang/leetcode/L81_Search.java
@@ -3,6 +3,7 @@ package com.wufuqiang.leetcode;
 import javax.xml.stream.events.EndDocument;
 
 /**
+ *  搜索旋转排序数组 II
  * @ author wufuqiang
  **/
 public class L81_Search {
@@ -16,14 +17,17 @@ public class L81_Search {
         }
         int mid = (low+high)/2;
         if(nums[low] == nums[mid]){
+            //nums[low] == nums[mid]无法判断转折点在那一边，但low这个位置肯定不是目标值
             return searchRec(nums,low+1,high,target);
         }else if(nums[low] < nums[mid]){
+            //若nums[low] < nums[mid]，则左边是有序的，转折点在右边
             if(nums[low] <= target && target < nums[mid]){
                 return searchRec(nums,low,mid-1,target);
             }else{
                 return searchRec(nums,mid,high,target);
             }
         }else{
+            //右边是有序的，转折点在左边
             if(nums[mid] < target && target <= nums[high]){
                 return searchRec(nums,mid+1,high,target);
             }else{
