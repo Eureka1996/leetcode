c4d7dee0e1d6b5b6e34c75cbdcf93d9a9b0e906b
1527198976RIJ <1527198976rij@kuaishou.com>
2020-08-15 17:44:42 +0800

    L3_无重复字符的最长子串

--- src/main/java/com/wufuqiang/leetcode/L3_LengthOfLongestSubstring.java
+++ src/main/java/com/wufuqiang/leetcode/L3_LengthOfLongestSubstring.java
@@ -4,21 +4,25 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-//窗口函数
+/**
+ * 无重复字符的最长子串
+ * 窗口函数+双指针
+ */
 public class L3_LengthOfLongestSubstring {
 	public int lengthOfLongestSubstring(String s) {
 		int res = 0;
 		if(s == null || s.length() == 0) return res;
 		Set<Character> window = new HashSet<>();
-		boolean flag = true;
 		int left = 0;
 		int right = 0;
 		while(right < s.length()){
 			if(!window.contains(s.charAt(right))){
+				//窗口中不包含这个字符，则加入到窗口，然后右指针右移
 				window.add(s.charAt(right));
 				res = Math.max(res,window.size());
 				right+=1;
 			}else{
+				//窗口中包含了这个字符
 				window.remove(s.charAt(left));
 				left+=1;
 			}
