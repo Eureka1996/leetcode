428fadb537942126494a6a0353a2e349aed5b949
1527198976RIJ <1527198976rij@kuaishou.com>
2020-08-26 22:01:47 +0800

    L155_最小栈，添加注释

--- src/main/java/com/wufuqiang/leetcode/L155_MinStack.java
+++ src/main/java/com/wufuqiang/leetcode/L155_MinStack.java
@@ -11,22 +11,28 @@ public class L155_MinStack {
 
 class MinStack {
 
+    //实际的栈操作
     Stack<Integer> data ;
+    //记录最小值的栈操作
     Stack<Integer> helper ;
 
     /** initialize your data structure here. */
     public MinStack() {
         data = new Stack<Integer>();
+        //此栈记录当前的最小值
         helper = new Stack<Integer>();
     }
 
+    //入栈
     public void push(int x) {
-        data.add(x);
+        data.push(x);
+        //新进来的值和栈顶做比较，如果小于等于栈顶，则入栈，更新最小值
         if(helper.isEmpty() || helper.peek()>=x){
-            helper.add(x);
+            helper.push(x);
         }
     }
 
+    //出栈
     public void pop() {
         if(!data.isEmpty()){
             int top = data.pop();
