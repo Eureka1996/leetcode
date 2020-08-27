2c3f7f6c531b743603120a32271634ebf6c48c24
1527198976RIJ <1527198976rij@kuaishou.com>
2020-08-27 11:35:01 +0800

    L53_添加注释

--- src/main/java/com/wufuqiang/leetcode/L53_MaxSubArray.java
+++ src/main/java/com/wufuqiang/leetcode/L53_MaxSubArray.java
@@ -1,13 +1,28 @@
 package com.wufuqiang.leetcode;
 //最大连续子数组
 public class L53_MaxSubArray {
+	/**
+	 * 分治的方法
+	 * @param nums
+	 * @return
+	 */
 	public int maxSubArray(int[] nums) {
 		int result = 0;
 		result = sumSubArray(nums,0,nums.length-1);
 		return result;
 	}
 
+
+
+	/**
+	 *
+	 * @param nums 给定的数组
+	 * @param left 给定的最左边的数组索引，包含
+	 * @param right 给定的最右边的数组索引，包含
+	 * @return
+	 */
 	public int sumSubArray(int[] nums,int left ,int right){
+
 		if(left>right) return Integer.MIN_VALUE;
 		if(left==right) return nums[left];
 		int mid = (left+right)/2;
@@ -17,6 +32,14 @@ public class L53_MaxSubArray {
 		return Math.max(Math.max(leftMax,rightMax),crossMax);
 	}
 
+	/**
+	 * 包含中间两个节点的最大值
+	 * @param nums
+	 * @param left
+	 * @param mid
+	 * @param right
+	 * @return
+	 */
 	private int crossSumMax(int[] nums, int left, int mid, int right) {
 		if(right-left<1) return Integer.MIN_VALUE;
         int sum = nums[mid]+nums[mid+1];
@@ -33,6 +56,20 @@ public class L53_MaxSubArray {
         	if(tmp > sum) sum = tmp;
         }
 		return sum;
+	}
+
+	/**
+	 * 动态规划的方法
+	 * @param nums
+	 * @return
+	 */
+	public int maxSubArray2(int[] nums) {
+		int result = nums[0];
+		for(int i =1;i<nums.length;i++){
+			nums[i] += Math.max(nums[i-1],0);
+			result = Math.max(result,nums[i]);
+		}
+		return result;
 	}
 
 }
