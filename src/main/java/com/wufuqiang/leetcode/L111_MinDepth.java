bd062fc5e1374b8077303fa8dbf11b71c382efc8
1527198976RIJ <1527198976rij@kuaishou.com>
2020-08-27 14:33:07 +0800

    L111_最小高度_添加注释

--- src/main/java/com/wufuqiang/leetcode/L111_MinDepth.java
+++ src/main/java/com/wufuqiang/leetcode/L111_MinDepth.java
@@ -6,7 +6,7 @@ public class L111_MinDepth {
 
 
 
-    public int minDepth(TreeNode root) {
+    public int minDepth2(TreeNode root) {
         if(root == null){
             return 0;
         }
@@ -23,5 +23,19 @@ public class L111_MinDepth {
         }
         return minDepth+1;
 
+    }
+
+    public int minDepth(TreeNode root) {
+        //如果是空节点，高度为0
+        if(root == null) return 0;
+
+        //如果是叶子节点，高度为1
+        if(root.left == null && root.right == null) return 1;
+
+        int leftDepth = Integer.MAX_VALUE;
+        if(root.left != null) leftDepth = minDepth(root.left);
+        int rightDepth = Integer.MAX_VALUE;
+        if(root.right != null) rightDepth = minDepth(root.right);
+        return Math.min(leftDepth,rightDepth)+1;
     }
 }
