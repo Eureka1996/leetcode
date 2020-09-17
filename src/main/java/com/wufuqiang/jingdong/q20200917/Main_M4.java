12592a7c1b19e9bf027540b1d00e53d06c943cd0
1527198976RIJ <1527198976rij@kuaishou.com>
2020-09-17 11:33:52 +0800

    京东秋招笔试

---
+++ src/main/java/com/wufuqiang/jingdong/q20200917/Main_M4.java
@@ -0,0 +1,12 @@
+package com.wufuqiang.jingdong.q20200917;
+
+import java.util.Scanner;
+
+public class Main_M4 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+		sc.close();
+	}
+}
---
+++ src/main/java/com/wufuqiang/jingdong/q20200917/Main_W2.java
@@ -0,0 +1,12 @@
+package com.wufuqiang.jingdong.q20200917;
+
+import java.util.Scanner;
+
+public class Main_W2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+		sc.close();
+	}
+}
