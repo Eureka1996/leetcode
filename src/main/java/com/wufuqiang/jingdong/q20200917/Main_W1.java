025b39237ebf847e3fedf7f8709bbd783bdc9d15
1527198976RIJ <1527198976rij@kuaishou.com>
2020-09-17 11:48:25 +0800

    京东秋招笔试

--- src/main/java/com/wufuqiang/jingdong/q20200917/Main_W1.java
+++ src/main/java/com/wufuqiang/jingdong/q20200917/Main_W1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main_W1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
+		System.out.println("wufuqiang");
 
 		sc.close();
 	}
