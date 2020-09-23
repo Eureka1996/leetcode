44ba7390d6f3f6fd7a0f9737c8421eab988ea765
1527198976RIJ <1527198976rij@kuaishou.com>
2020-09-23 20:31:08 +0800

    跟谁学秋招笔试

---
+++ src/main/java/com/wufuqiang/genshuixue/q20200923/Main_M1.java
@@ -0,0 +1,12 @@
+package com.wufuqiang.genshuixue.q20200923;
+
+import java.util.Scanner;
+
+public class Main_M1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+		sc.close();
+	}
+}
