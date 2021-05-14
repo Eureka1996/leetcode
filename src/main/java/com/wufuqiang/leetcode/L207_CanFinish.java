d7739e68ee0ca6d5b698a52055b0090b109b2bd6
Fuqiang Wu <wufuqiang@buaa.edu.cn>
2021-05-14 22:20:04 +0800

    ADD：207，添加注释

--- src/main/java/com/wufuqiang/leetcode/L207_CanFinish.java
+++ src/main/java/com/wufuqiang/leetcode/L207_CanFinish.java
@@ -1,13 +1,20 @@
 package com.wufuqiang.leetcode;
 
-import java.util.LinkedList;
+import java.util.*;
 
+/**
+ * 课程表
+ * 拓扑排序
+ */
 public class L207_CanFinish {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        //用于记录每个课程的入度，数组索引为课程编号，值为入度
         int[] degrees = new int[numCourses];
         for(int[] p : prerequisites){
             degrees[p[0]] = degrees[p[0]]+1;
         }
+
+        //用于记录入度为0的课程
         LinkedList<Integer> queue = new LinkedList<>();
         for(int i = 0 ; i<numCourses ;i++){
             if(degrees[i] == 0){
@@ -25,4 +32,46 @@ public class L207_CanFinish {
         }
         return numCourses == 0;
     }
+
+    public boolean canFinish02(int numCourses, int[][] prerequisites) {
+        //用于记录每个课程的入度，数组索引为课程编号，值为入度
+        int[] degrees = new int[numCourses];
+
+        //key为课程编号，value为依赖它的课程编号
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int[] p : prerequisites){
+            degrees[p[0]] = degrees[p[0]]+1;
+            List<Integer> c = map.getOrDefault(p[1], new ArrayList<>());
+            c.add(p[0]);
+            map.put(p[1],c);
+        }
+
+        //用于记录入度为0的课程
+        LinkedList<Integer> queue = new LinkedList<>();
+        for(int i = 0 ; i<numCourses ;i++){
+            if(degrees[i] == 0){
+                queue.addLast(i);
+            }
+        }
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while (size > 0){
+                Integer pre = queue.pollFirst();
+                size--;
+                numCourses--;
+                List<Integer> cs = map.get(pre);
+                if(cs == null) continue;
+                for(int i:cs){
+                    degrees[i]--;
+                    if(degrees[i] == 0){
+                        queue.offerLast(i);
+                    }
+                }
+            }
+        }
+        return numCourses == 0;
+    }
+
+
+
 }
