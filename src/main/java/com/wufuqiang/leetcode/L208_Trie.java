52a820c0234f89db2bfa3b04f02654672071a339
Fuqiang Wu <wufuqiang@buaa.edu.cn>
2021-05-23 18:58:48 +0800

    ADD：208. 实现 Trie (前缀树)

---
+++ src/main/java/com/wufuqiang/leetcode/L208_Trie.java
@@ -0,0 +1,72 @@
+package com.wufuqiang.leetcode;
+
+/**
+ * @author: Wu Fuqiang
+ * @create: 2021-05-23 18:49
+ */
+public class L208_Trie {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Trie {
+
+    private Trie[] children;
+    private boolean isEnd;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Trie node = this;
+        for(int i = 0;i<word.length();i++){
+            char c = word.charAt(i);
+            int index = c-'a';
+            if(node.children[index] == null){
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix){
+        Trie node = this;
+        for(int i =0 ;i<prefix.length();i++){
+            char c = prefix.charAt(i);
+            int index = c - 'a';
+            if(node.children[index] == null){
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
