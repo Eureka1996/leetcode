27cceccfb5ae1c6c135505c0cadb901c1d07203c
Fuqiang Wu <wufuqiang@buaa.edu.cn>
2021-05-31 13:47:34 +0800

    ADD：前序遍历，增加递归解法

--- src/main/java/com/wufuqiang/leetcode/L144_PreorderTraversal.java
+++ src/main/java/com/wufuqiang/leetcode/L144_PreorderTraversal.java
@@ -7,9 +7,10 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * 迭代，前序遍历
+ * 迭代，前序遍历，根左右
  */
 public class L144_PreorderTraversal {
+	//迭代前序遍历
 	public List<Integer> preorderTraversal(TreeNode root) {
 		List<Integer> result = new ArrayList<>();
 		LinkedList<TreeNode> stack = new LinkedList<>();
@@ -27,4 +28,21 @@ public class L144_PreorderTraversal {
 
 		return result;
 	}
+
+	//递归前序遍历
+	public List<Integer> preorderTraversal2(TreeNode root){
+		List<Integer> result = new ArrayList<>();
+
+		preorder(root,result);
+
+		return result;
+	}
+
+	public void preorder(TreeNode root ,List<Integer> result){
+		if(root == null) return;
+		result.add(root.val);
+		preorder(root.left,result);
+		preorder(root.right,result);
+	}
+
 }
