6d18bd78eee00f8f2c6f93fca863fd01b4780097
Fuqiang Wu <wufuqiang@buaa.edu.cn>
2021-06-12 17:29:29 +0800

    ADD：1449. 数位成本和为目标值的最大数字

---
+++ src/main/java/com/wufuqiang/leetcode/L1449_LargestNumber.java
@@ -0,0 +1,54 @@
+package com.wufuqiang.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * @author: Wu Fuqiang
+ * @create: 2021-06-12 17:19
+ */
+public class L1449_LargestNumber {
+    public static void main(String[] args) {
+
+    }
+    public String largestNumber(int[] cost, int target) {
+        int[][] dp = new int[10][target+1];
+        for(int i =0;i<10;i++){
+            Arrays.fill(dp[i],Integer.MIN_VALUE);
+        }
+        int[][] from = new int[10][target+1];
+        dp[0][0] = 0;
+        for(int i = 0;i<9;i++){
+            int c = cost[i];
+            for(int j =0;j<=target;j++){
+                if(j < c){
+                    dp[i+1][j] = dp[i][j];
+                    from[i+1][j] = j;
+                }else{
+                    if(dp[i][j] > dp[i+1][j-c] +1){
+                        dp[i+1][j] = dp[i][j];
+                        from[i+1][j] = j;
+                    }else{
+                        dp[i+1][j] = dp[i+1][j-c]+1;
+                        from[i+1][j] = j-c;
+                    }
+                }
+            }
+        }
+
+        if(dp[9][target] < 0) return "0";
+
+        StringBuffer sb = new StringBuffer();
+        int i = 9;
+        int j = target;
+        while(i>0){
+            if(j==from[i][j]){
+                i--;
+            }else{
+                sb.append(i);
+                j=from[i][j];
+            }
+        }
+        return sb.toString();
+
+    }
+}
