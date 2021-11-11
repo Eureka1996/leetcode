46982f0018ce5351220228f4a8e7fdb710f4a99d
wufuqiang <wufuqiang@qq.com>
2021-11-11 10:54:15 +0800

    ADD:魔数，第K个值

--- src/main/java/com/wufuqiang/leetcode/mianshiti/MS17_09_GetKthMagicNumber.java
+++ src/main/java/com/wufuqiang/leetcode/mianshiti/MS17_09_GetKthMagicNumber.java
@@ -1,4 +1,4 @@
-package com.wufuqiang.leetcode;
+package com.wufuqiang.leetcode.mianshiti;
 
 public class MS17_09_GetKthMagicNumber {
 	public int getKthMagicNumber(int k) {
@@ -8,10 +8,13 @@ public class MS17_09_GetKthMagicNumber {
 		if(k == 1){
 			return 1;
 		}
+		// 下标值
 		int index3 = 0;
 		int index5 = 0;
 		int index7 = 0;
+		// 初始化结果数组
 		int[] result = new int[k];
+		// 初始值为1
 		result[0] = 1;
 		for(int i = 1;i<k;i++){
 			int num3 = result[index3] * 3;
@@ -19,6 +22,7 @@ public class MS17_09_GetKthMagicNumber {
 			int num7 = result[index7] * 7;
 			int min = Math.min(Math.min(num3, num5), num7);
 			result[i] = min;
+			// 判断需要移动的位置下标
 			if(num3 == min){
 				index3+=1;
 			}
