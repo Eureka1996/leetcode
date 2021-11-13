680a4f72874a7e83318e6fc122aa05119b599954
wufuqiang <wufuqiang@qq.com>
2021-11-13 22:50:40 +0800

    MOD:0-1背包

--- src/main/java/com/wufuqiang/dynamicprogramming/Knapsack01.java
+++ src/main/java/com/wufuqiang/dynamicprogramming/Knapsack01.java
@@ -7,6 +7,14 @@ public class Knapsack01 {
 
 	public static void main(String[] args) {
 		Knapsack01 knapsack01 = new Knapsack01();
+		int n = 4;
+		int m = 5;
+		int[] costs = new int[]{2, 4, 4, 5};
+		int[] volumes = new int[]{1, 2, 3, 4};
+		
+		int res1 = knapsack01.knapsack01(n,m,costs,volumes);
+		System.out.println(res1);
+		
 		int res = knapsack01.knapsack02(4, 5,new int[]{2, 4, 4, 5}, new int[]{1, 2, 3, 4});
 		System.out.println(res);
 	}
@@ -22,17 +30,20 @@ public class Knapsack01 {
 	 */
 	public int knapsack01(int n,int m,int[] cost,int[] volume){
 		//每行表示考虑到第i个物品，每列表示背包有多少容量
+		// dp[i][j]表示只看前i个物品，总体积是j的情况下，总价值最大是多少。
 		int[][] dp = new int[n+1][m+1];
+		// i=0时，表示没有物品，所以所有值为0
+		// j=0时，表示背包容量为0，所以所有值为0
 		for(int i = 1;i<=n;i++){
 			for(int j = 1;j<=m;j++){
-				//不选当前物品
+				//不选第i个物品，即当前物品
 				dp[i][j] = dp[i-1][j];
-				if(j-volume[i-1]>=0){
+				// volumn[i-1]表示第i个物品的容量
+				if(j-volume[i-1]>=0){ // 判断是否有足够容量选第i个物品
 					//选第i个物品
 					dp[i][j] = Math.max(dp[i][j],cost[i-1]+dp[i-1][j-volume[i-1]]);
 				}
 				System.out.print(dp[i][j]+"\t");
-
 			}
 			System.out.println();
 		}
@@ -57,6 +68,7 @@ public class Knapsack01 {
 		for(int i = 1;i<=n;i++){
 			//从后往前遍历，原有的数据是i-1个物品时的情况
 			for(int j = m;j>=volume[i-1];j--){
+				// dp[j]为不选的最大值
 				dp[j] = Math.max(dp[j],dp[j-volume[i-1]]+cost[i-1]);
 			}
 		}
