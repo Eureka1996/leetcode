dd2b18b08a995c63d32348d6d1820d8b8d062de8
wufuqiang <wufuqiang@qq.com>
2021-11-14 15:01:09 +0800

    MOD:518. 零钱兑换 II

--- src/main/java/com/wufuqiang/leetcode/L518_Change.java
+++ src/main/java/com/wufuqiang/leetcode/L518_Change.java
@@ -18,4 +18,41 @@ public class L518_Change {
         }
         return dp[amount];
     }
+
+    /**
+     * 二维数组解法
+     * @param amount
+     * @param coins
+     * @return
+     */
+    public int change2(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n+1][amount+1];
+        for(int i = 0 ;i<= amount;i++){
+            dp[0][i] = 0;
+        }
+        for(int i = 0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1;i<= n;i++){
+            for(int j = 1;j<= amount;j++){
+                dp[i][j] = dp[i-1][j];
+                if(j-coins[i-1] >= 0)
+                    dp[i][j] += dp[i][j-coins[i-1]];
+            }
+        }
+        return dp[n][amount];
+    }
+
+    public int change3(int amount, int[] coins) {
+        int n = coins.length;
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+        for(int i = 0;i<n;i++){
+            for(int a = coins[i];a<= amount;a++){
+                dp[a] += dp[a-coins[i]];
+            }
+        }
+        return dp[amount];
+    }
 }
